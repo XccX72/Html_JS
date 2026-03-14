f1e6d88064a3654d8058a9fb67e899eb8a7e0391
XccX72 <danni.spriet800215@gmail.com>
2026-03-14 20:28:04 +0100

    ssdsd

--- Learning/testing.js
+++ Learning/testing.js
@@ -1,1 +1,47 @@
 console.log("hello world");
+
+console.error
+
+let danni_er_gul = true;
+
+while(danni_er_gul){    
+    console.log("dann")
+}        
+
+/*
+for d in(danni_er_gul){
+    console.log("isak brun henriksen")
+}
+*/
+
+const tulletall = 10; // dette er en konstant variabel :D
+let ape = 1; // denne kan du endre på :)
+
+// 3 forskjellige deler
+for(ape; ape < tulletall; ape++){
+    console.log("wasdwasd");
+}
+
+const night = ['ridwan', 'isak', 'erlend']
+
+/*
+night.forEach(documents){
+    console.log(document);
+}
+    */
+
+const day = {"isak": true, "Danni": true}
+
+// funksjoner
+
+// lag funksjon
+function sayHello(){
+    console.log("Hello");
+}
+
+
+
+
+
+// kjør funksjon
+sayHello();
